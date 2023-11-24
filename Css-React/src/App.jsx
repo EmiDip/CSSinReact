37cdf2d7394b63e1dc80e84b91d117b97a2f6e69
EmiDip <di.palma.emilie@outlook.fr>
2023-11-24 16:33:44 +0100

    creation composants

--- Css-React/src/App.jsx
+++ Css-React/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { CardSass } from "./components/CardSass";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <CardSass />
+    </div>
+  );
 }
 
 export default App;
